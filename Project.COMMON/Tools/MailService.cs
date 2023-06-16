316a3809c26efd80691b85d22f2dd63ff27c9397
Furkan Demir <Furkan Demir@FURKAN>
2023-06-16 16:20:59 +0300

    MailService,Şifreleme sınıflari oluşturuldu ve gerekli kodlar entegre edildi

---
+++ Project.COMMON/Tools/MailService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.COMMON.Tools
+{
+    public static class MailService
+    {
+        //Kullanılacak Mail Adresi ve şifresi girilecek  gmail tipinde...
+
+        public static void Send(string receiver,string password="Şifre yeri", string body ="Onay Maili",string subject="Email Testi",string sender ="Mail Adresi")
+        {
+            MailAddress senderEmail = new MailAddress(sender);
+            MailAddress receiverEmail = new MailAddress(receiver);
+
+
+            SmtpClient smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(senderEmail.Address, password)
+            };
+
+            using (MailMessage message = new MailMessage(senderEmail, receiverEmail)
+            {
+                Subject = subject,
+                Body = body,
+            })
+            {
+                smtp.Send(message);
+
+            }
+
+
+
+
+        }
+    }
+}
