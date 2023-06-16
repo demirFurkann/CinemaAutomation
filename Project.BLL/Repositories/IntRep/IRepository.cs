8bce337dbb3d076442ad4f69ddfa7adef0279240
Furkan Demir <Furkan Demir@FURKAN>
2023-06-16 15:44:09 +0300

    Bll de Crud işlemleri yapıldı

---
+++ Project.BLL/Repositories/IntRep/IRepository.cs
@@ -0,0 +1,41 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Repositories.IntRep
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        // Listeleme işlemleri
+        List<T> GetAll();
+        List<T> GetActives();
+        List<T> GetPassives();
+        List<T> GetModifieds();
+
+        // Crud işlemleri
+
+        void Add(T item);
+        void Update(T item);
+        void Delete(T item);
+        void Destroy(T item);
+
+
+        // Linq sorgulari
+
+        List<T> Where(Expression<Func<T, bool>> exp);
+        bool Any(Expression<Func<T, bool>> exp);
+        T FirstOrDefault(Expression<Func<T, bool>> exp);
+        object Select(Expression<Func<T, object>> exp);
+        IQueryable<X> Select<X>(Expression<Func<T, X>> exp);
+
+        // Find
+
+        T Find(int id);
+
+    }
+}
